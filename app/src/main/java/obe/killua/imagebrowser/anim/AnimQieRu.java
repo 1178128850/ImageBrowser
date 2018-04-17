58ee02a1c49c8f2663393be3d38a4ccb4153f4f9
wangtao <wangtao@orange-i.com>
2018-04-17 16:33:21 +0800

    ui to optimize

---
+++ app/src/main/java/obe/killua/imagebrowser/anim/AnimQieRu.java
@@ -0,0 +1,25 @@
+package anim;
+
+import android.graphics.Canvas;
+import android.graphics.Region;
+import android.view.View;
+
+import com.wangpm.enteranimation.EnterAnimLayout;
+
+/**
+ * Created by wpm on 2017/3/30.
+ */
+
+public class AnimQieRu extends Anim {
+    public AnimQieRu(EnterAnimLayout view) {
+        super(view);
+    }
+
+    @Override
+    public void handleCanvas(Canvas canvas, float rate) {
+
+        canvas.translate(0,h-h*rate);
+
+        canvas.save();
+    }
+}
